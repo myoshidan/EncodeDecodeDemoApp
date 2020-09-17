015780c40b8fdeb1f8bc277c830b7a2c2e324a87
myoshidan <ma.yoshida.dev@gmail.com>
2020-09-18 02:14:14 +0900

    Create WPF Project and Implementation minimum code

---
+++ EncodeDecodeDemoApp/EncodeDecodeDemoApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace EncodeDecodeDemoApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void encode_Click(object sender, RoutedEventArgs e)
+        {
+            var utf8 = new UTF8Encoding();
+            var data = utf8.GetBytes(input.Text);
+            var ret = Convert.ToBase64String(data);
+            result.Text = ret;
+        }
+
+        private void decode_Click(object sender, RoutedEventArgs e)
+        {
+            var utf8 = new UTF8Encoding();
+            var ret = utf8.GetString(Convert.FromBase64String(result.Text));
+            input.Text = ret;
+        }
+    }
+}
